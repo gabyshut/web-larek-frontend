182b76f9fee16d6fefc032e2d8ce670661e7d9ee
unknown <gabriella.jester@yandex.ru>
2025-07-12 18:04:24 +0300

    Редактирование документации

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export interface ICardItemAPI {
+export interface ICardItem {
     id: string;
     description: string;
     image: string;
@@ -7,40 +7,47 @@ export interface ICardItemAPI {
     price: number;
 }
 
-export interface IBasket{
-    getProducts(): IItemView[];
-    deleteItem(item: IItemView): void;
-    addItem(itemId: string): void;
+export type TProductId = Pick<ICardItem, 'id'>
+
+export interface IBasket {
+    items: ICardItem[];
+    total: number;
+    getProducts(): ICardItem[];
+    deleteItem(itemId: TProductId): void;
+    addItem(itemId: TProductId): void;
+    clearList(items: ICardItem[]): void;
 }
+
+export interface ICardData {
+    items: ICardItem[];
+    getItemById(id: TProductId): ICardItem | undefined;
+}
+
+export interface IUserOrderData {
+    payment: TPaymentMethod;
+    address: string;
+    email: string;
+    phone: string;
+    products: ICardItem[];
+    total: number;
+}
+
+export type TPaymentMethod = 'card' | 'cash';
+
 
 export interface IModal {
     open(content: HTMLElement): void;
     close(): void;
   }  
   
-export interface IItemView {
-  id: string;
-  title: string;
-  category: string;
-  price: number;
-  imageUrl: string;
-}
-  
-export interface IOrder {
-  payment: string;
-  address: string;
-  email: string;
-  phone: string;
-  items: string[];
-}
   
 export interface IApiClient {
-  getProducts(): Promise<ICardItemAPI[]>;
-  createOrder(order: IOrder): Promise<void>;
+  getProducts(): Promise<ICardItem[]>;
+  createOrder(order: IUserOrderData): Promise<void>;
 }
   
 export type AppEvents = 
-    | { type: 'product:add'; payload: ICardItemAPI }
+    | { type: 'product:add'; payload: ICardItem }
     | { type: 'product:remove'; payload: string }
     | { type: 'basket:open' }
     | { type: 'modal:open'; payload: HTMLElement }
